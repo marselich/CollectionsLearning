484bbc644e9240061687b396d85880e60677adfb
marselich <marsellkaliev7722@gmail.com>
2022-07-01 18:50:59 +0500

    LinkedList bio example

--- src/ru/sinergy/Main.java
+++ src/ru/sinergy/Main.java
@@ -1,14 +1,11 @@
 package ru.sinergy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        //Arrays
         Cat[] cats = new Cat[3];
 
         cats[0] = new Cat("Мурка");
@@ -19,6 +16,8 @@ public class Main {
 
         System.out.println(Arrays.toString(cats));
 
+
+        //ArrayList
         ArrayList<Cat> catsList = new ArrayList();
 
         for(Cat cat : cats) {
@@ -42,6 +41,25 @@ public class Main {
         System.out.println(catsList.toString());
 
         System.out.println(catsList.size());
+
+
+        //LinkedList
+
+        String str1 = new String("Hello world");
+        String str2 = new String("My name is Marsell");
+        String str3 = new String("I love java");
+
+        LinkedList<String> myBio = new LinkedList();
+
+        myBio.add(str1);
+        myBio.add(str2);
+        myBio.add(str3);
+
+        System.out.println(myBio);
+
+        myBio.remove();
+
+        System.out.println(myBio);
 
     }
 }
