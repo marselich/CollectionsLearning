0c47822f219460c39b6fe61514a212c186e46f9f
marselich <marsellkaliev7722@gmail.com>
2022-07-01 17:41:52 +0500

    cats example

--- src/ru/sinergy/Main.java
+++ src/ru/sinergy/Main.java
@@ -1,7 +1,19 @@
 package ru.sinergy;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Cat[] cats = new Cat[3];
+
+        cats[0] = new Cat("Мурка");
+        cats[1] = new Cat("Барсик");
+        cats[2] = new Cat("Черныш");
+
+       // cats[1] = null;
+
+        System.out.println(Arrays.toString(cats));
     }
 }
